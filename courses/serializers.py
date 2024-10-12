3adbc3a1d981329f58cf4659965e1ae29414a35e
DuckRainbow <sillestyna@gmail.com>
2024-10-12 19:06:24 +0500

    lessons views and urls

--- courses/serializers.py
+++ courses/serializers.py
@@ -6,10 +6,10 @@ from courses.models import Lesson, Course
 class CourseSerializer(ModelSerializer):
     class Meta:
         model = Course
-        fields = ['title', 'preview', 'description']
+        fields = ['id', 'title', 'preview', 'description']
 
 
 class LessonSerializer(ModelSerializer):
     class Meta:
         model = Lesson
-        fields = ['title', 'preview', 'description', 'video_link', 'course']
+        fields = ['id', 'title', 'preview', 'description', 'video_link', 'course']
