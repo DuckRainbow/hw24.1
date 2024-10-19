9367a4c80eb546039edbe27e0cca33fbe21d994f
DuckRainbow <sillestyna@gmail.com>
2024-10-19 18:54:58 +0500

    half of 3rd task 25.1

--- courses/views.py
+++ courses/views.py
@@ -15,10 +15,20 @@ class CourseViewSet(ModelViewSet):
             return CourseDetailSerializer
         return CourseSerializer
 
+    def perform_create(self, serializer):
+        course = serializer.save()
+        course.creator = self.request.user
+        course.save()
+
 
 class LessonCreateAPIView(CreateAPIView):
     queryset = Lesson.objects.all()
     serializer_class = LessonSerializer
+
+    def perform_create(self, serializer):
+        lesson = serializer.save()
+        lesson.creator = self.request.user
+        lesson.save()
 
 
 class LessonListAPIView(ListAPIView):
