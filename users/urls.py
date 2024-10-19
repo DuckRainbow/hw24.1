b56c26f3db17fca22e328caa0a542f05481f4fca
DuckRainbow <sillestyna@gmail.com>
2024-10-19 18:08:26 +0500

    1 task 25.1

--- users/urls.py
+++ users/urls.py
@@ -1,18 +1,25 @@
 from django.urls import path
+from rest_framework.permissions import AllowAny
 from rest_framework.routers import DefaultRouter
 
 from users.apps import UsersConfig
 from users.views import (UserCreateAPIView, UserListAPIView, UserUpdateAPIView, UserRetrieveAPIView,
                          UserDestroyAPIView, PaymentListAPIView)
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 app_name = UsersConfig.name
 
 router = DefaultRouter()
 
 urlpatterns = [
-    path('create/', UserCreateAPIView.as_view(), name='user_create'),
+    path('login/', TokenObtainPairView.as_view(permission_classes=(AllowAny,)), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(permission_classes=(AllowAny,)), name='token_refresh'),
+    path('register/', UserCreateAPIView.as_view(), name='user_create'),
     path('', UserListAPIView.as_view(), name='user_list'),
-    path('update/<int:pk>/', UserUpdateAPIView.as_view(), name='user_update'),
+    path('update/<int:pk>/', UserUpdateAPIView.as_view(), name='register'),
     path('<int:pk>/', UserRetrieveAPIView.as_view(), name='user_retrieve'),
     path('delete/<int:pk>', UserDestroyAPIView.as_view(), name='user_delete'),
     path('payments/', PaymentListAPIView.as_view(), name='payment_list'),
--- users/views.py
+++ users/views.py
@@ -3,12 +3,21 @@ from rest_framework.generics import (CreateAPIView, DestroyAPIView,
                                      ListAPIView, RetrieveAPIView,
                                      UpdateAPIView)
 from rest_framework.filters import SearchFilter, OrderingFilter
+from rest_framework.permissions import AllowAny
+
 from users.models import User, Payment
 from users.serializers import PaymentSerializer, UserSerializer
 
 
 class UserCreateAPIView(CreateAPIView):
     serializer_class = UserSerializer
+    queryset = User.objects.all()
+    permission_classes = (AllowAny,)
+
+    def perform_create(self, serializer):
+        user = serializer.save(is_active=True)
+        user.set_password(user.password)
+        user.save()
 
 
 class UserListAPIView(ListAPIView):
