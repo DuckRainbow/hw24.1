75c7c6c494fad5767ef89ead32361e34c3701865
DuckRainbow <sillestyna@gmail.com>
2024-10-27 18:10:03 +0500

    4th task 25.2

--- courses/validators.py
+++ courses/validators.py
@@ -4,18 +4,12 @@ from rest_framework.serializers import ValidationError
 class LessonsVideoValidator:
 
     def __init__(self, field):
-        allowed_links = [
-            "https://www.youtube.com/",
-            "https://youtube.com/",
-        ]
         self.field = field
-        self.allowed_links = allowed_links
 
     def __call__(self, value):
         """Проверяем введенную ссылку на соответсвие с ссылками в списке разрешенных ресурсов"""
-        if any(link in value for link in self.allowed_links):
-            return True
-        else:
-            raise ValidationError(
-                f"Допустимо использовать только ссылки на следующие ресурсы: {*self.allowed_links,}"
-            )
+        url = "http://youtube.com"
+        if value.get("url_video"):
+            if url not in value.get("url_video"):
+                raise ValidationError("Необходимо присутствие ссылки на youtube.")
+        return None
